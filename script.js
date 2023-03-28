413a2363503fd508a59b28d542151b201b6183ba
Harrison Clark <hc@rainman-collateral.com>
2023-03-28 16:53:42 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const inputArrayElement = document.getElementById("input-array");
+const outputElement = document.getElementById("output");
+
+inputArrayElement.addEventListener("input", () => {
+  const inputString = inputArrayElement.value;
+  const arrayString = inputString.trim().replace(/'/g, '"');
+
+  try {
+    const array = JSON.parse(arrayString);
+    outputElement.innerHTML = "";
+
+    if (Array.isArray(array)) {
+      const cards = document.createElement("div");
+      cards.style.display = "flex";
+      cards.style.flexWrap = "wrap";
+      cards.style.alignItems = "stretch";
+
+      for (let i = 0; i < array.length; i++) {
+        const card = document.createElement("div");
+        card.className = "card";
+        card.style.flex = "0 0 auto";
+
+        const cardTitle = document.createElement("div");
+        cardTitle.className = "card-title";
+        cardTitle.textContent = `${i}`;
+
+        const cardContent = document.createElement("div");
+
+        if (typeof array[i] === "object") {
+          const pre = document.createElement("pre");
+          pre.textContent = JSON.stringify(array[i], null, 2);
+          cardContent.appendChild(pre);
+        } else {
+          cardContent.textContent = array[i];
+        }
+
+        card.appendChild(cardTitle);
+        card.appendChild(cardContent);
+        cards.appendChild(card);
+      }
+      outputElement.appendChild(cards);
+    } else {
+      outputElement.textContent =
+        "Invalid array format. Please paste a valid array.";
+    }
+  } catch (error) {
+    outputElement.textContent =
+      "Error: Unable to parse the input. Make sure it is a valid array.";
+  }
+});
